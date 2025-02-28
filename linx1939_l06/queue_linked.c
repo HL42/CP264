4704327ee4853cfe391cacb8ac5fccb44e281b81
HL42 <fuquanlin347@gmail.com>
2025-02-28 09:19:33 -0500

    Implement queue_linked functions and stack_linked functions

--- linx1939_l06/queue_linked.c
+++ linx1939_l06/queue_linked.c
@@ -18,14 +18,41 @@ queue_linked *queue_initialize()
 {
 
     // your code here
+    queue_linked *queue = malloc(sizeof(queue_linked));
 
-    return NULL;
+    if (queue == NULL)
+    {
+        return NULL;
+    }
+
+    queue->count = 0;
+    queue->front = NULL;
+    queue->rear = NULL;
+
+    return queue;
 }
 
 void queue_free(queue_linked **source)
 {
 
     // your code here
+    if (source == NULL || *source == NULL)
+    {
+        return;
+    }
+
+    queue_node *current = (*source)->front;
+
+    while (current != NULL)
+    {
+        queue_node *temp = current;
+        data_free(&temp->item);
+        current = current->next;
+        free(temp);
+    }
+
+    free(*source);
+    *source = NULL;
 
     return;
 }
@@ -34,8 +61,7 @@ bool queue_empty(const queue_linked *source)
 {
 
     // your code here
-
-    return true;
+    return source == NULL || source->front == NULL;
 }
 
 int queue_count(const queue_linked *source)
@@ -43,14 +69,48 @@ int queue_count(const queue_linked *source)
 
     // your code here
 
-    return 0;
+    return source == NULL ? 0 : source->count;
 }
 
 bool queue_insert(queue_linked *source, data_ptr item)
 {
 
     // your code here
+    if (source == NULL)
+    {
+        return false;
+    }
 
+    queue_node *new_node = malloc(sizeof(queue_node));
+
+    if (new_node == NULL)
+    {
+        return false;
+    }
+
+    new_node->item = item;
+
+    if (new_node->item == NULL)
+    {
+        data_free(&new_node->item);
+        return false;
+    }
+
+    data_copy(new_node->item, item);
+    new_node->next = NULL;
+
+    if (source->rear == NULL)
+    {
+        source->front = new_node;
+        source->rear = new_node;
+    }
+    else
+    {
+        source->rear->next = new_node;
+        source->rear = new_node;
+    }
+
+    source->count++;
     return true;
 }
 
@@ -58,7 +118,11 @@ bool queue_peek(const queue_linked *source, data_ptr item)
 {
 
     // your code here
-
+    if (source == NULL || source->front == NULL)
+    {
+        return false; // 队列为空
+    }
+    data_copy(item, source->front->item); // 复制头部数据
     return true;
 }
 
@@ -66,6 +130,32 @@ bool queue_remove(queue_linked *source, data_ptr *item)
 {
 
     // your code here
+    if (source == NULL || source->front == NULL)
+    {
+        return false;
+    }
+
+    queue_node *temp = source->front;
+
+    *item = (data_ptr)malloc(sizeof(**item));
+
+    if (*item == NULL)
+    {
+        return false;
+    }
+
+    data_copy(*item, temp->item);
+
+    source->front = temp->next;
+
+    if (source->front == NULL)
+    {
+        source->rear = NULL;
+    }
+
+    data_free(&temp->item);
+    free(temp);
+    source->count--;
 
     return true;
 }
